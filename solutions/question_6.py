8d64ee81e51980b4836dec0db8e0b5f38e3729f3
Peggy Fan <pfan@sf-pfan02-mac.lan>
2021-03-09 08:47:33 -0800

    revised solution files

---
+++ solutions/question_6.py
@@ -0,0 +1,14 @@
+# 4. Write a function in Python that takes in a dataset (.csv file)
+#    and a number of days forward, and generates predictions for hourly
+#    order volume.
+
+def forecast_volume(data_file, days_forward):
+  
+  # your code here; see additional comments in question_4.r for assistance
+  # with formatting, etc.
+  
+  return(data_out)
+
+
+
+
